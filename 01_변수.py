9e941743c25130e0fb2c6c984667c858480055a9
Sunset-kim <wood2002@naver.com>
2022-03-06 22:58:49 +0900

    01. 파이썬 변수, 타입

---
+++ 01_변수.py
@@ -0,0 +1,22 @@
+# 변수와 타입
+# 변수: 변하는 값을 넣는 공간
+x = 1
+y = 2
+z = x + y
+print(z);
+
+# 타입
+# int => 정수
+# float => 분수, 소수
+
+# boolean - 대문자
+print(type(True))
+
+# string
+x1 = 'ab'
+y2 = '2'
+
+print(x1 + y2);
+
+# type casting 변수의 타입을 변경
+print(type(str(3)));
